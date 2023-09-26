09cc067bb122bba35a1b51fd354d2501a5b82fc7
IagoMelo01 <iagomello160@gmail.com>
2023-09-26 14:29:09 -0300

    integração dos serviços

---
+++ app/conexao.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Conexao {
+
+  private $host = 'localhost';
+  private $database = 'php_pdo';
+  private $user = 'root';
+  private $pass = '';
+  
+
+  public function conectar(){
+    try{
+
+    } catch()
+  }
+}
---
+++ app/tarefa.model.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Tarefa {
+  private $id;
+  private $id_status;
+  private $tarefa;
+  private $data_cadastro;
+
+  public function __get($attr){
+    return $this->$attr;
+  }
+}
