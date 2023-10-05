44b104be527e9becb7f846bc38bd643b162c06f3
IagoMelo01 <iagomello160@gmail.com>
2023-10-05 12:43:26 -0300

    atualização de tarefas

--- public/todas_tarefas.php
+++ public/todas_tarefas.php
@@ -13,6 +13,39 @@ require 'tarefa_controller.php';
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<script>
+			function editar(id){
+				// criar um form de edição
+				let form = document.createElement('form');
+				form.action = "#"
+				form.method = 'post'
+				form.className = 'row'
+
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.className = 'col-9 form-control'
+
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				form.appendChild(inputTarefa)
+				form.appendChild(button)
+
+				// selecionar a div
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				// limpar o texto da tarefa
+				tarefa.innerHTML = ''
+
+				// incluir form
+				tarefa.insertBefore(form, tarefa[0])
+			}
+		</script>
+	
 	</head>
 
 	<body>
@@ -44,11 +77,11 @@ require 'tarefa_controller.php';
 
 								<?php foreach($tarefas as $key => $tarefa) {  ?>
 									<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?= $tarefa->tarefa; ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>"><?= $tarefa->tarefa; ?> (<?= $tarefa->status ?>)</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
-											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
-											<i class="fas fa-check-square fa-lg text-success"></i>
+											<i class="fas fa-trash-alt fa-lg text-danger" onclick="excluir()"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, <?= $tarefa->tarefa; ?>)"></i>
+											<i class="fas fa-check-square fa-lg text-success" onclick="concluida()"></i>
 										</div>
 									</div>
 
